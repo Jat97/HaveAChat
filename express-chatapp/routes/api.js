2df2c82fe5bc8cbdd5fe57a75c28ac4a5d3fcb9f
Jat97 <85377239+Jat97@users.noreply.github.com>
2026-05-04 02:04:40 -0400

    Split up endpoints for handling chat requests and blocking users; removed search endpoint; edited logout method from PUT to PATCH

--- express-chatapp/routes/api.js
+++ express-chatapp/routes/api.js
@@ -12,37 +12,33 @@ router.post('/login', userController.log_in);
 
 router.get('/users', userController.user_index);
 
-router.patch('/profile/picture', upload.single('profilepicture'), userController.change_profile_picture);
+router.patch('/user/picture', upload.single('profilepicture'), userController.change_profile_picture);
 
-router.get('/friends', userController.get_friends);
+router.delete('/:username/unfriend', userController.remove_from_friendslist);
 
-router.get('/blocked', userController.get_blocked_users);
+router.post('/:username/block', userController.block_user);
 
-router.delete('/unfriend/:username', userController.remove_from_friendslist);
+router.delete('/:username/unblock', userController.unblock_users);
 
-router.post('/block/:username', userController.toggle_block_users);
-
-router.delete('/unblock/:username', userController.toggle_block_users);
-
-router.patch('/user/hidden/toggle', userController.toggle_hidden_status);
-
-router.get('/search?:query', userController.search_users);
+router.patch('/user/hidden', userController.toggle_hidden_status);
 
 router.delete('/user', userController.delete_account);
 
 router.get('/user', userController.get_logged_data);
 
-router.put('/user/logout', userController.log_out);
+router.patch('/user/logout', userController.log_out);
 
 //Chat Functions //
 
 router.get('/chats', chatController.get_chats);
 
-router.get('/:username/chat', chatController.get_user_messages);
-
 router.post('/:username/message', upload.single('chatimage'), chatController.send_message);
 
-router.put('/chat/:requestid/request/handle', chatController.handle_chat_request);
+router.post('/chat/:username', chatController.initiate_chat);
+
+router.post('/chat/:requestid/accept', chatController.accept_chat_request);
+
+router.delete('/chat/:requestid/reject', chatController.reject_chat_request);
 
 router.delete('/:chatid/chat', chatController.delete_chat);
 
