3b5d66b70201bf669920ab49c4a9ead709072738
Jat97 <85377239+Jat97@users.noreply.github.com>
2026-05-05 11:35:27 -0400

    Added conditional for onClick handler

--- vite-chatapp/src/Routes/Inputs/Search/Search.jsx
+++ vite-chatapp/src/Routes/Inputs/Search/Search.jsx
@@ -10,7 +10,7 @@ const Search = (props) => {
         <div className='flex justify-end items-center w-3/4 md:justify-center md:w-3/4'>
             <input data-testid='user_search' id='user_search' className='bg-slate-200 border-2 border-slate-200 rounded-full max-w-md w-full md:p-1  focus:bg-white' 
                 placeholder='Find someone to chat with...' onChange={searchUsers} 
-                onClick={account_tab ? setAccountTab : null}>
+                {...(account_tab && {onClick: setAccountTab})}>
             </input>
         </div>
     )
